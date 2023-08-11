dd328229d54b26f71a2998bf67d858ea101ee345
Kam <karadhak@adobe.com>
2023-08-12 05:40:01 +1000

    HERO Banner Updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ import {
   loadCSS,
 } from './lib-franklin.js';
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
+const LCP_BLOCKS = ['hero']; // add your LCP blocks to the list
 
 /**
  * Builds hero block and prepends to main in a new section.
@@ -22,10 +22,17 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  const href = main.querySelector('a');
+  const p = main.querySelector('p');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    const teaser = document.createElement('div');
+    teaser.classList.add("teaser");
+    teaser.append(h1);
+    teaser.append(p);
+    teaser.append(href);
+    section.append(buildBlock('hero', { elems: [picture, teaser] }));
     main.prepend(section);
   }
 }
